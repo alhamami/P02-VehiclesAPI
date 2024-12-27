5aa42fefc0edd910b4bf1c286211356b89127ca6
alhamami <jalalalhamami@gmail.com>
2024-12-27 22:21:36 +0300

    Added documentation to CarController for swagger

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -8,10 +8,14 @@ import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.service.CarService;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
-
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiParam;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +28,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
 /**
  * Implements a REST-based controller for the Vehicles API.
  */
+
+
 @RestController
 @RequestMapping("/cars")
 class CarController {
@@ -43,6 +52,14 @@ class CarController {
      * Creates a list to store any vehicles.
      * @return list of vehicles
      */
+    @ApiOperation(value = "Creates a list to store any vehicles", notes = "Creates a list to store any vehicles and return list of vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized Request"),
+            @ApiResponse(code = 403, message = "Forbidden Request"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 503, message = "Service Unavailable")})
     @GetMapping
     Resources<Resource<Car>> list() {
         List<Resource<Car>> resources = carService.list().stream().map(assembler::toResource)
@@ -56,14 +73,25 @@ class CarController {
      * @param id the id number of the given vehicle
      * @return all information for the requested vehicle
      */
+    @ApiOperation(value = "Gets information of a specific car by ID", notes = "Gets information of a specific car by ID and return all information for the requested vehicle")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized Request"),
+            @ApiResponse(code = 403, message = "Forbidden Request"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 503, message = "Service Unavailable")})
     @GetMapping("/{id}")
-    Resource<Car> get(@PathVariable Long id) {
+    Resource<Car> get(@ApiParam(value  = "id is the id number of the given vehicle") @PathVariable Long id) {
         /**
          * TODO: Use the `findById` method from the Car Service to get car information.
          * TODO: Use the `assembler` on that car and return the resulting output.
          *   Update the first line as part of the above implementing.
          */
-        return assembler.toResource(new Car());
+        Car foundCar = carService.findById(id);
+        Resource<Car> resource = assembler.toResource(foundCar);
+
+        return resource;
     }
 
     /**
@@ -72,14 +100,23 @@ class CarController {
      * @return response that the new vehicle was added to the system
      * @throws URISyntaxException if the request contains invalid fields or syntax
      */
+    @ApiOperation(value = "Posts information to create a new vehicle in the system", notes = "Posts information to create a new vehicle in the system and response that the new vehicle was added to the system")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized Request"),
+            @ApiResponse(code = 403, message = "Forbidden Request"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 503, message = "Service Unavailable")})
     @PostMapping
-    ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
+    ResponseEntity<?> post(@ApiParam(value = "car is a new vehicle to add to the system") @Valid @RequestBody Car car) throws URISyntaxException {
         /**
          * TODO: Use the `save` method from the Car Service to save the input car.
          * TODO: Use the `assembler` on that saved car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+        Car newCar = carService.save(car);
+        Resource<Car> resource = assembler.toResource(newCar);
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
@@ -89,15 +126,35 @@ class CarController {
      * @param car The updated information about the related vehicle.
      * @return response that the vehicle was updated in the system
      */
+    @ApiOperation(value = "Updates the information of a vehicle in the system", notes = "Updates the information of a vehicle in the system and response that the vehicle was updated in the system")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized Request"),
+            @ApiResponse(code = 403, message = "Forbidden Request"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 503, message = "Service Unavailable")})
     @PutMapping("/{id}")
-    ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
+    ResponseEntity<?> put(@ApiParam(value = "id is the ID number for which to update vehicle information") @PathVariable Long id,@ApiParam(value = "car is the updated information about the related vehicle") @Valid @RequestBody Car car) {
         /**
          * TODO: Set the id of the input car object to the `id` input.
          * TODO: Save the car using the `save` method from the Car service
          * TODO: Use the `assembler` on that updated car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+
+        Car upadteCar = new Car();
+        upadteCar.setId(id);
+        upadteCar.setCreatedAt(car.getCreatedAt());
+        upadteCar.setModifiedAt(car.getModifiedAt());
+        upadteCar.setCondition(car.getCondition());
+        upadteCar.setDetails(car.getDetails());
+        upadteCar.setLocation(upadteCar.getLocation());
+        upadteCar.setPrice(car.getPrice());
+
+        Car updatedCar = carService.save(car);
+
+        Resource<Car> resource = assembler.toResource(updatedCar);
         return ResponseEntity.ok(resource);
     }
 
@@ -106,11 +163,22 @@ class CarController {
      * @param id The ID number of the vehicle to remove.
      * @return response that the related vehicle is no longer in the system
      */
+    @ApiOperation(value = "Removes a vehicle from the system", notes = "Removes a vehicle from the system and response that the related vehicle is no longer in the system")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized Request"),
+            @ApiResponse(code = 403, message = "Forbidden Request"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 503, message = "Service Unavailable")})
     @DeleteMapping("/{id}")
-    ResponseEntity<?> delete(@PathVariable Long id) {
+    ResponseEntity<?> delete(@ApiParam(value = "id is the ID number of the vehicle to remove") @PathVariable Long id) {
         /**
          * TODO: Use the Car Service to delete the requested vehicle.
          */
+
+        carService.delete(id);
+
         return ResponseEntity.noContent().build();
     }
 }
